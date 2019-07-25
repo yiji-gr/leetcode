31c473e9204ef11fb5aef0f9297d8cc3b55752ef
yiji-gr <516123131@qq.com>
2019-07-25 15:54:41 +0800

    Create balanced-binary-tree.cpp

---
+++ balanced-binary-tree.cpp
@@ -0,0 +1,32 @@
+//  2019年7月25日
+//  leetcode第110题 https://leetcode-cn.com/problems/balanced-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool flag = true;
+    
+    int dfs(TreeNode *root){
+        if(!root)
+            return 0;
+        
+        int l = dfs(root->left);
+        int r = dfs(root->right);
+        if(abs(l - r) > 1)
+            flag = false;
+        return 1 + max(l, r);
+    }
+    
+    bool isBalanced(TreeNode* root) {
+        dfs(root);
+        return flag;
+    }
+};
