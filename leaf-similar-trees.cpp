988b0c75589f623f7e21d2215a9b4226574ce48d
yiji-gr <516123131@qq.com>
2019-08-10 17:14:24 +0800

    Create leaf-similar-trees.cpp

---
+++ leaf-similar-trees.cpp
@@ -0,0 +1,36 @@
+//  2019年8月10日
+//  leetcode第872题 https://leetcode-cn.com/problems/leaf-similar-trees/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void get_leaf(TreeNode* rt, vector<int>& leafs) {
+        if(nullptr == rt)    
+            return;
+        if(rt->left == nullptr && rt->right == nullptr)
+            leafs.push_back(rt->val);
+        get_leaf(rt->left, leafs);
+        get_leaf(rt->right, leafs);
+    }
+    
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int> leafs1, leafs2; 
+        get_leaf(root1, leafs1);
+        get_leaf(root2, leafs2);
+        
+        if(leafs1.size() != leafs2.size())
+            return false;
+        for(int i = 0; i < leafs1.size(); ++i)
+            if(leafs1[i] != leafs2[i])
+                return false;
+        return true;
+    }
+};
